916dec1af0ca0b748fffbeb7b8e20e6cb2ec4d5b
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-28 21:06:45 -0300

    Add client and server class

---
+++ src/models/client.py
@@ -0,0 +1,17 @@
+import socket
+
+BUFFER_SIZE = 1024
+
+
+class Client:
+    def __init__(self, name: str):
+        self.name = name
+        self.buffer_size = BUFFER_SIZE
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect_to(self, server_ip: str, server_port: int):
+        self.socket.connect((server_ip, server_port))
+
+    def deposit(self, amount: float):
+        return
---
+++ src/models/server.py
@@ -0,0 +1,35 @@
+import socket
+
+BUFFER_SIZE = 1024
+
+
+class Server:
+    def __init__(self, port: int):
+        self.ip = self._get_own_ip()
+        self.port = port
+        self.buffer_size = BUFFER_SIZE
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def bind_socket(self):
+        self.socket.bind((self.ip, self.port))
+
+    def _get_own_ip(self):
+        aux_socket = None
+        try:
+            # Create a UDP socket
+            aux_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+            # Connect to an extern IP
+            # 8.8.8.8 IP is a public DNS from Google
+            aux_socket.connect(("8.8.8.8", 1))
+
+            # Get local IP associated with this socket
+            own_ip = aux_socket.getsockname()[0]
+        except Exception as e:
+            print(f"Couldn't get local IP: {e}")
+        finally:
+            if aux_socket:
+                aux_socket.close()
+
+        return own_ip
