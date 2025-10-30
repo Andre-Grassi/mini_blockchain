5a76bccb72656da4cb89acf296f08a14ed10bff5
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-29 21:40:03 -0300

    Add hash computing methods

--- src/models/server.py
+++ src/models/server.py
@@ -1,21 +1,31 @@
 import socket
+from typing import Optional
 from network_node import NetworkNode
-
-BUFFER_SIZE = 1024
+from block import Block
+from models.acc_creation_block import AccCreationBlock
 
 
 class Server(NetworkNode):
     def __init__(self, port: int):
         super().__init__()
-        
+
         self.ip = self._get_own_ip()
         self.port = port
-        self.buffer_size = BUFFER_SIZE
+        self.block_chain = []
 
     def bind_socket(self):
         self.socket.bind((self.ip, self.port))
 
-    def _get_own_ip(self):
+    def compute_hash(self, block: Block, last_hash: str) -> str:
+        return "hash placeholder"
+
+    def compute_genesis_hash(self, block: Block):
+        if self.block_chain.count > 0 and block != self.block_chain[0]:
+            raise RuntimeError(
+                "Cannot compute hash of a block without last hash, since genesis has already been calculated"
+            )
+
+    def _get_own_ip(self) -> str:
         aux_socket = None
         try:
             # Create a UDP socket
