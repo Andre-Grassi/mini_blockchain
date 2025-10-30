df244aa70fd26d06b27ffbe774209090548b014d
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-29 21:39:40 -0300

    Remove BUFFER_SIZE constant and associated attribute from Client class

--- src/models/client.py
+++ src/models/client.py
@@ -1,15 +1,12 @@
 import socket
 from network_node import NetworkNode
-
-BUFFER_SIZE = 1024
-
+from operation import Operation
 
 class Client(NetworkNode):
     def __init__(self, name: str):
         super().__init__()
 
         self.name = name
-        self.buffer_size = BUFFER_SIZE
 
     def connect_to(self, server_ip: str, server_port: int):
         self.socket.connect((server_ip, server_port))
