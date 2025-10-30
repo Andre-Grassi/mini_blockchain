0f7773689049427489b0a3ac7651ea5f03c6c6d0
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-29 22:01:52 -0300

    Fix constructor parameter type for hash in AccCreationBlock

--- src/models/acc_creation_block.py
+++ src/models/acc_creation_block.py
@@ -5,7 +5,7 @@ from block import Block
 
 
 class AccCreationBlock(Block):
-    def __init__(self, owner_name: str, amount: float, date: datetime, hash_s: str):
-        super().__init__(owner_name, Operation.DEPOSIT, amount, hash_s)
+    def __init__(self, owner_name: str, amount: float, date: datetime, hash_b: bytes):
+        super().__init__(owner_name, Operation.DEPOSIT, amount, hash_b)
 
         self.date = date
