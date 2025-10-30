dfed03e6e54b9a1eb4bd81b00b9ac3128a205c0a
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-29 22:08:15 -0300

    Refactor parse_message to use Operation enum for actions

--- src/models/network_node.py
+++ src/models/network_node.py
@@ -1,5 +1,6 @@
 import socket
 from typing import Optional
+from operation import Operation
 
 BUFFER_SIZE = 1024
 
@@ -14,22 +15,32 @@ class NetworkNode:
 
     def parse_message(self, message: str):
         parts = message.strip().split()
+
+        # Check if message has incorrect structure
         if len(parts) < 1 or len(parts) > 2:
             return None
 
         action = parts[0].lower()
 
+        # Check if the action is recognizable
         if action not in ("deposit", "withdraw", "q"):
             return None
 
         if action == "q":
-            return (action, 0)
+            return (Operation.QUIT, 0)
 
         amount_s = parts[1]
 
+        # Try to convert string amount to float
         try:
             amount = float(amount_s)
         except ValueError:
             return None
 
-        return (action, amount)
+        # Return correct operation according to action
+        if action == "deposit":
+            return (Operation.DEPOSIT, amount)
+        elif action == "withdraw":
+            return (Operation.WITHDRAW, amount)
+
+        raise RuntimeError("Unknown error while parsing message")
