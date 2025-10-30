96f2af03d9829a3bc94a1cc82b2b3dd44cf6f664
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-30 13:37:57 -0300

    Remove hash parameter from AccCreationBlock constructor

--- src/models/acc_creation_block.py
+++ src/models/acc_creation_block.py
@@ -5,7 +5,8 @@ from block import Block
 
 
 class AccCreationBlock(Block):
-    def __init__(self, owner_name: str, amount: float, date: datetime, hash_b: bytes):
-        super().__init__(owner_name, Operation.DEPOSIT, amount, hash_b)
+    def __init__(self, owner_name: str, amount: float, date: datetime):
+        # An account creation must always be a deposit
+        super().__init__(owner_name, amount, Operation.DEPOSIT)
 
         self.date = date
