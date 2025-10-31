77d24d416fc9cdb386f72cbd3e3f1a8c8238ac6b
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-30 21:36:14 -0300

    Refactor server message handling to improve operation parsing and client interaction

--- src/run_server.py
+++ src/run_server.py
@@ -2,6 +2,7 @@
 
 import argparse
 from models.server import Server
+from models.operation import Operation
 
 
 def main(server_port: int):
@@ -17,7 +18,9 @@ def main(server_port: int):
     connection, client_address = server.socket.accept()
 
     # Keep the connection alive, exchanging messages, until it's closed
-    while True:
+    is_open = True
+    client_name = None
+    while is_open:
         message_bytes = connection.recv(server.buffer_size)
 
         if not message_bytes:
@@ -26,6 +29,27 @@ def main(server_port: int):
         message = message_bytes.decode()
 
         print(f"received data: {message}")
+
+        (operation, op_data) = server.parse_message(message)
+
+        if operation is None:
+            server.send_str("Unknow operation.")
+        elif operation == Operation.QUIT:
+            is_open = False
+        elif operation == Operation.NAME:
+            client_name = op_data
+        elif client_name is None:
+            server.send_str("First, send your name: name <your_name>")
+
+        # Money operations
+        elif op_data <= 0:
+            server.send_str("Amount must be positive.")
+        elif operation == Operation.DEPOSIT:
+            server.client_deposit(client_name, op_data)
+        elif operation == Operation.WITHDRAW:
+            raise NotImplementedError("Withdraw not implemented")
+        else:
+            raise RuntimeError("Unknown error")
 
     connection.close()
     server.close()
