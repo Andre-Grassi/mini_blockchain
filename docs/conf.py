ac0445e1dc71c787c6ab41820bc8ac08092c56f7
Andre-Grassi <andregrassi.j@gmail.com>
2025-10-31 19:33:35 -0300

    Fix modules path

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src/models"))
+sys.path.insert(0, os.path.abspath("../src"))
 
 project = "Mini Blockchain"
 copyright = "2025, Andre Grassi, Ricardo Faria"
